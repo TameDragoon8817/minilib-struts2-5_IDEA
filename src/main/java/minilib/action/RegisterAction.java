6717a1e52b5c37734a64f68789a37fa36f1d7fd6
TameDragoon8817 <jcz20010219@gmail.com>
2021-12-05 19:06:38 +0800

    getset function

--- src/main/java/minilib/action/RegisterAction.java
+++ src/main/java/minilib/action/RegisterAction.java
@@ -1,4 +1,53 @@
 package minilib.action;
 
-public class RegisterAction {
+import com.opensymphony.xwork2.ActionSupport;
+
+public class RegisterAction extends ActionSupport {
+    private String identifynumber;
+    private String nickname;
+    private String sex;
+    private String username;
+    private String userpass;
+
+    public String getIdentifynumber() {
+        return identifynumber;
+    }
+
+    public void setIdentifynumber(String identifynumber) {
+        this.identifynumber = identifynumber;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserpass() {
+        return userpass;
+    }
+
+    public void setUserpass(String userpass) {
+        this.userpass = userpass;
+    }
+
+
 }
