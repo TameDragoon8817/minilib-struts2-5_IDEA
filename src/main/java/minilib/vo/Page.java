7b5bdb7e38fe6ee4b115d60aadc289339b67cc1b
TameDragoon8817 <jcz20010219@gmail.com>
2021-12-06 18:09:23 +0800

    preview

---
+++ src/main/java/minilib/vo/Page.java
@@ -0,0 +1,22 @@
+package minilib.vo;
+
+public class Page {
+    public String getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(String currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public String getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(String totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public String currentPage;
+    public String totalPage;
+}
